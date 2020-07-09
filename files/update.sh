2919e8fdb65c90d1b29bfad0a9bc119782bed626
Nicsena <Nicholassena8190@gmail.com>
2020-07-08 21:57:15 -0700

    Added Updater for the update-redis.sh file and it refreshes the project.

--- files/update.sh
+++ files/update.sh
@@ -1,1 +1,15 @@
-
+# The updater of the update-redish.sh file
+echo "Update is 95% Completed"
+cd /app/
+wget -q https://raw.githubusercontent.com/Nicsena/redis-server-on-glitch-assets/master/files/update-redis.sh -O update-redis.sh
+echo "Update is 100% Completed"
+echo "Refreshing Project shortly..."
+wait 3
+echo "Refreshing in 3..."
+wait 1
+echo "Refreshing in 2..."
+wait 1
+echo "Refreshing in 1..."
+wait 1
+echo "Refreshing!"
+refresh
